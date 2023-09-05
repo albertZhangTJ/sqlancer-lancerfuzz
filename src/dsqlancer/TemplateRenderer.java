a9dea85ef512b406ae2902c2c2f9df6746126d66
albertZhangTJ <tongjunz@outlook.com>
2023-09-05 13:19:49 +0800

    Minor progress

--- src/dsqlancer/TemplateRenderer.java
+++ src/dsqlancer/TemplateRenderer.java
@@ -44,10 +44,42 @@ public class TemplateRenderer {
         }
     }
 
+    // The name is kinda misleading but i cannot come up with a better one for the moment being
+    // It's not "replacing" rather prepending value to each tag, the tags will be removed after the whole template is 
+    // The reason for not directly removing the tag is that one tag might be assigned multiple values 
+    // e.g. the <SCHEMA_NODE\> tag in the fuzzer template corresponds to not one but multiple functions
     private String replace_tag(String template, String key, String value){
         String tag = "<"+key+"/>";
         int length = tag.length()+value.length();
-        return null;
+        for (int cursor = 0; cursor<template.length()-tag.length(); cursor++){
+            if (template.substring(cursor, cursor+tag.length()).equals(tag)){
+                template = template.substring(0,cursor) + value + template.substring(cursor);
+                cursor += value.length();
+            }
+        }
+        return template;
+    }
+
+    // This is a rather simple String matching (no parsing here)
+    // This procedure will look for each occurence of the string "\>" and search back forward for the nearest "<", remove what's between
+    // Also for now spaces in tags are disallowed so any occurence of space before finding the corresponding left bracket will be considered as syntax error
+    private String strip_tags(String template){
+        String left_bracket = "<";
+        String right_bracket = "/>";
+        while (template.indexOf(right_bracket)!=-1){
+            int right_idx = template.indexOf(right_bracket);
+            int left_idx = template.substring(0, right_idx).lastIndexOf(left_bracket);
+            // just a bit of sanity check here, no guarantee
+            if (left_idx==-1){
+                Utils.panic("TemplateRenderer::strip_tags : cannot find left bracket for tag");
+            }
+            if (template.substring(left_idx, right_idx).indexOf(" ")!=-1){
+                Utils.panic("TemplateRenderer::strip_tags : found a tag name with space");
+            }
+
+            template = template.substring(0, left_idx) + template.substring(right_idx+2);
+        }
+        return template;
     }
 
     public String render(GrammarGraph graph, List<Stage> stages, List<DBMSOption> options){
