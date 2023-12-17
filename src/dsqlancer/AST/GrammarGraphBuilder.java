db4cc225490d5ef69a57932a0bf348133ccc4c3e
albertZhangTJ <tongjunz@outlook.com>
2023-12-17 16:44:08 +0800

    Add fuzzer rendering for RP_ID

--- src/dsqlancer/AST/GrammarGraphBuilder.java
+++ src/dsqlancer/AST/GrammarGraphBuilder.java
@@ -650,6 +650,7 @@ public class GrammarGraphBuilder {
         graph.process_expected_errors();
         graph.process_weights();
         graph.process_repetition_limits();
+        graph.process_repetition_ids();
         // Ignored the options for the graph, not entirely sure why grammar graph need info on options
         // Will come back to implement if later found needed
         // TODO
--- src/dsqlancer/TemplateRenderer.java
+++ src/dsqlancer/TemplateRenderer.java
@@ -257,6 +257,7 @@ public class TemplateRenderer {
             template = replace_tag(template, "MIN_DEPTH", ""+n.get_min_depth());
             template = replace_tag(template, "QUAN_MIN", ""+n.get_min());
             template = replace_tag(template, "QUAN_MAX", ""+n.get_max());
+            template = replace_tag(template, "RPID", n.get_rp_id()==null ? "null" : "\""+n.get_rp_id()+"\"");
 
             for (Edge e : n.get_outward_edges()){
                 template = replace_tag(template, "CHILD_NODE_REF", "            ans = ans + " + gen_function_call(e.get_dest(), e)+ ";\n");
