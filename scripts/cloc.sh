261faa545b4b4024b27a4526f97cfe36fed92853
albertZhangTJ <tongjunz@outlook.com>
2024-01-28 16:41:58 +0800

    Update script for cloc

--- scripts/cloc.sh
+++ scripts/cloc.sh
@@ -1,8 +1,27 @@
-#!/bin/sh
-for D in src/sqlancer/*; do
+#!/bin/bash
+cd src/sqlancer
+str=$'DBMS,LOC\n'
+for D in *; do
+    # count 
     if [ -d "${D}" ]; then
-        echo "Counting ${D}"
-        cloc "${D}" | grep "Java" 
-        echo "\n"
+        loc=$(cloc "${D}" | grep "Java" | awk '{print $5}')
+        # There are a couple of folders that are smaller than 1000 LOCs
+        # They are either not DBMS-spceific code (e.g. any, transformations)
+        # Or are simply wraps over another DBMS (cosmos, timescale, etc.)
+        # Th
+        if [ "$loc" -gt 1000 ]; then
+            echo "Counting $D"
+            str+="$D,"
+            str+=$loc
+            str+=$'\n'
+        else
+            echo "Skipping $D"
+        fi
     fi
 done
+cd ../..
+echo "$str" > loc_results.csv
+
+# remove trailing newline
+printf "%s" "$(< loc_results.csv)" > tmp
+mv tmp loc_results.csv
