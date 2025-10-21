bedf978e5f4d981accacab46bdbd6ca134330555
Albert Zhang <albertz44944@gmail.com>
2025-10-21 17:27:08 +0100

    Fix NPE

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -2,5 +2,5 @@
 echo "If you need commands to clean up stuff after stopping"
 echo "Add it to scripts/cleanup.sh"
 
-docker stop postgres-dev
-docker rm postgres-dev
+sudo docker stop postgres-dev
+sudo docker rm postgres-dev
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-cp src/sqlancer/any/Fuzzer.java.template.txt src/sqlancer/any/Fuzzer.java && \
-#First build, to get the SGL jar
-echo "Building Seagull" && \
-rm -f Fuzzer.java && \
-mvn clean package -Dskip.main=true -DskipTests && \
-#Generate fuzzer java code from grammar file and configuration file
-echo "Starting Seagull" && \
-java -jar target/SGL-jar-with-dependencies.jar -c $2 -g $1 && \
-echo "Fuzzer generated" && \
-#cat Fuzzer.java && \
-mv Fuzzer.java src/sqlancer/any/Fuzzer.java && \
-#Second build with the real Fuzzer code, to get the sqlancer jar
-./scripts/db_setup.sh
-echo "Building SQLancer" && \
-mvn clean package -Dskip.SGL=true -DskipTests && \
-cd target && \
-echo "Starting SQLancer" && \
-java -jar sqlancer-jar-with-dependencies.jar -g && \
-# java -XX:+FlightRecorder -XX:StartFlightRecording=duration=2h,filename=profile.jfr,settings=profile -jar sqlancer-jar-with-dependencies.jar -g && \
-cd .. && \
-echo "Finished, cleaning up" && \
 cp src/sqlancer/any/Fuzzer.java.template.txt src/sqlancer/any/Fuzzer.java
-./scripts/cleanup.sh
+#First build, to get the SGL jar
+echo "Building Seagull" 
+rm -f Fuzzer.java 
+mvn clean package -Dskip.main=true -DskipTests 
+#Generate fuzzer java code from grammar file and configuration file
+echo "Starting Seagull" 
+java -jar target/SGL-jar-with-dependencies.jar -c $2 -g $1 
+echo "Fuzzer generated" 
+#cat Fuzzer.java && \
+mv Fuzzer.java src/sqlancer/any/Fuzzer.java 
+#Second build with the real Fuzzer code, to get the sqlancer jar
+#./scripts/db_setup.sh
+echo "Building SQLancer" 
+mvn clean package -Dskip.SGL=true -DskipTests 
+cd target 
+echo "Starting SQLancer" 
+java -jar sqlancer-jar-with-dependencies.jar -g 
+# java -XX:+FlightRecorder -XX:StartFlightRecording=duration=2h,filename=profile.jfr,settings=profile -jar sqlancer-jar-with-dependencies.jar -g && \
+cd .. 
+echo "Finished, cleaning up" 
+cp src/sqlancer/any/Fuzzer.java.template.txt src/sqlancer/any/Fuzzer.java
+#./scripts/cleanup.sh
