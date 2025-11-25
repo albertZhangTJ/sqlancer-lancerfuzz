d27ecfae4847949ed1a8c54ce526048cd5099577
Albert Zhang <albertz44944@gmail.com>
2025-11-25 09:02:59 +0000

    Update postgres config

--- scripts/run.sh
+++ scripts/run.sh
@@ -16,7 +16,7 @@ echo "Building SQLancer"
 mvn clean package -Dskip.SGL=true -DskipTests 
 cd target 
 echo "Starting SQLancer" 
-java -jar sqlancer-jar-with-dependencies.jar -g 
+timeout 12h java -jar sqlancer-jar-with-dependencies.jar -g 
 # java -XX:+FlightRecorder -XX:StartFlightRecording=duration=2h,filename=profile.jfr,settings=profile -jar sqlancer-jar-with-dependencies.jar -g && \
 cd .. 
 echo "Finished, cleaning up" 
