a23701fc69a0b2357f120990c4661e44b142ab3a
Albert Zhang <albertz44944@gmail.com>
2026-03-22 18:29:39 +0000

    Update plotting R code

--- scripts/plot_loc.R
+++ scripts/plot_loc.R
@@ -1,4 +1,4 @@
-# install.packages("svglite")
+install.packages("svglite",  repos = "https://cloud.r-project.org", configure.args="--with-freetype-include=/usr/include/freetype2")
 library(svglite)
 data <- read.csv("loc_results.csv")
 svglite("loc.svg", width=10, height=5)
